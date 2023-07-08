0e33a2553fd508366595fceca1352ece251dfa1f
dmitry.razumov.nb <dmitry.razumov@novsu.ru>
2023-07-09 02:31:28 +0300

    first commit sprint 9

---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -0,0 +1,90 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.exception.ValidationException;
+import java.time.LocalDate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class UserControllerTest {
+    private UserController userController;
+    User user;
+
+    @BeforeEach
+    void setUp() {
+        userController = new UserController();
+        user = new User( 1, "mail@mail.ru", "dolore"
+                , "Nick Name", LocalDate.of(1946, 8, 20));
+    }
+
+    @Test
+    void shouldCreateUser() {
+        assertEquals(userController.create(user).getId(), 1);
+    }
+
+    @Test
+    void shouldCreateUserWithEmptyName() {
+        user.setName("");
+
+        assertEquals(userController.create(user).getId(), 1);
+    }
+
+    @Test
+    void shouldCreateUserWithoutName() {
+        user.setName(null);
+
+        assertEquals(userController.create(user).getId(), 1);
+    }
+
+    @Test
+    void shouldFailLogin() {
+        user.setLogin("dolore ullamco");
+
+        assertThrows(ValidationException.class, () -> userController.create(user)
+                , "создался пользователь с пробелом в login");
+    }
+
+    @Test
+    void shouldFailEmail() {
+        user.setEmail("это-неправильный?эмейл@");
+
+        assertThrows(ValidationException.class, () -> userController.create(user)
+                , "создался пользователь с недопустимым email");
+    }
+
+    @Test
+    void shouldFailBirthday() {
+        user.setBirthday(LocalDate.of(2030,1,10));
+
+        assertThrows(ValidationException.class, () -> userController.create(user)
+                , "создался пользователь с будущей датой рождения");
+    }
+
+    @Test
+    void shouldUpdateUser() {
+        userController.create(user);
+        user.setLogin("doloreUpdate");
+
+        assertEquals(user, userController.update(user));
+    }
+
+    @Test
+    void shouldFailUpdateUnknownUser() {
+        userController.create(user);
+        user.setId(99);
+
+        assertThrows(ValidationException.class, () -> userController.update(user)
+                , "обновился пользователь с несуществующим id");
+    }
+
+    @Test
+    void shouldGetAllUsers() {
+        userController.create(user);
+        user.setLogin("secondU");
+        userController.create(user);
+
+        assertEquals(userController.getAll().size(), 2);
+    }
+}
